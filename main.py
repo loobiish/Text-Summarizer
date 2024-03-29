2265677d0d7a3b576248f92890178b829234de1c
Nikhil <nikhil@nsinha.me>
2024-03-29 17:02:44 +0530

    done till module data ingestion

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import os
 import sys
 sys.path.append('src')
 
-
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Welcome to basic log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
